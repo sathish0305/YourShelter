b029fb9e3262ae44d75d52d5a978d34c09ff2d08
Sathish <sathishms0305@gmail.com>
2022-10-10 22:40:14 +0530

    styling forms using bootstraps

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDb = async () =>{
     await Shelter.deleteMany({});
   for( let i=0;i<50;i++){
     const random1000 = Math.floor(Math.random() * 1000);
+    const price =Math.floor(Math.random() * 20) + 10;
    const camp =  new Shelter({
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
-        title: `${sample(descriptors)} ${sample(places)}`
+        title: `${sample(descriptors)} ${sample(places)}`,
+        image: 'https://source.unsplash.com/collection/1582764',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ipsa ratione, dignissimos deleniti et deserunt',
+        price
     }) 
     await camp.save();
   }
