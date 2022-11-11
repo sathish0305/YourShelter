d3707d7c92fa530a2f29ff1d812ae4233a9d8a78
Sathish <sathishms0305@gmail.com>
2022-11-11 10:35:51 +0530

    commit

--- middleware.js
+++ middleware.js
@@ -13,20 +13,20 @@ module.exports.isLoggedIn = (req, res, next) => {
     next();
 }
 
-module.exports.validateShelter = (req,res,next) => {
-    const {error} =  shelterSchema.validate(req.body);
-    if(error){
-      const msg = error.details.map(el => el.message).join(',')
-      throw new ExpressError(msg, 400)
-  } else {
-      next();
-  }
+module.exports.validateShelter = (req, res, next) => {
+    const { error } = shelterSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
 }
 
-module.exports.isAuthor = async(req,res,next) => {
+module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const shelter = await Shelter.findById(id);
-    if(!shelter.author.equals(req.user._id)){
+    if (!shelter.author.equals(req.user._id)) {
         req.flash('error', 'You do not have Permission!!!');
         return res.redirect(`/shelters/${id}`);
     }
@@ -34,20 +34,20 @@ module.exports.isAuthor = async(req,res,next) => {
 }
 
 module.exports.validateReview = (req, res, next) => {
-    const { error} = reviewSchema.validate(req.body);
-    if(error){
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
     } else {
         next();
     }
-    
+
 }
 
-module.exports.isReviewAuthor = async(req,res,next) => {
+module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
     const review = await Review.findById(reviewId);
-    if(!review.author.equals(req.user._id)){
+    if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have Permission!!!');
         return res.redirect(`/shelters/${id}`);
     }
--- schemas.js
+++ schemas.js
@@ -23,11 +23,11 @@ const extension = (joi) => ({
 
 const Joi = BaseJoi.extend(extension)
 
-module.exports.shelterSchema =  Joi.object({
+module.exports.shelterSchema = Joi.object({
     shelter: Joi.object({
         title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
-       // image: Joi.string().required(),
+        // image: Joi.string().required(),
         location: Joi.string().required().escapeHTML(),
         description: Joi.string().required().escapeHTML()
     }).required(),
