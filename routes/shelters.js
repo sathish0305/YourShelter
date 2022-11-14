b9c381aeea6d4cd96b2d063fd4ec31709d3153da
Sathish <sathishms0305@gmail.com>
2022-11-14 18:34:41 +0530

    clean up

--- routes/shelters.js
+++ routes/shelters.js
@@ -3,23 +3,25 @@ const router = express.Router();
 const shelters = require('../controllers/shelters');
 const catchAsync = require('../utils/catchAsync');
 const multer = require('multer');
-const {storage} = require('../cloudinary');
+const { storage } = require('../cloudinary');
 const upload = multer({ storage });
 
-const { isLoggedIn, validateShelter, isAuthor} = require('../middleware');
+const { isLoggedIn, validateShelter, isAuthor } = require('../middleware');
+const shelter = require('../models/shelter');
 
 router.route('/')
     .get(catchAsync(shelters.index))
-    .post(isLoggedIn,upload.array('image'),validateShelter, catchAsync(shelters.createShelter));
-    
+    .post(isLoggedIn, upload.array('image'), validateShelter, catchAsync(shelters.createShelter));
 
-router.get('/new', isLoggedIn , shelters.renderNewForm);
+
+router.get('/new', isLoggedIn, shelters.renderNewForm);
 
 router.route('/:id')
     .get(catchAsync(shelters.showShelter))
-    .put(isLoggedIn,isAuthor,upload.array('image'),validateShelter, catchAsync(shelters.updateShelter))
-    .delete(isLoggedIn,isAuthor,catchAsync(shelters.deleteShelter));
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateShelter, catchAsync(shelters.updateShelter))
+    .delete(isLoggedIn, isAuthor, catchAsync(shelters.deleteShelter));
 
- router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(shelters.renderEdit));
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(shelters.renderEdit));
 
- module.exports = router;
+
+module.exports = router;
